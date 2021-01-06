2cd3081442646ba11402630f9267a71eacf94eee
Duke Nukem <garrettduke87@gmail.com>
2021-01-06 14:41:08 -0700

    21.2 snapshot

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-//the connection handler is here rather than in server.js
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,35 +1,27 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-// import our typeDefs and resolvers
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
-// the connection from config/connection is imported here
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-// this ensures that every request performs an authentication 
-//check and the updated request object will be passed to the 
-//resolvers as the context
-const {authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-// integrate our Apollo server with the Express application and middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// this listens for the connection to be made, upon successful connection, the server is started.
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
